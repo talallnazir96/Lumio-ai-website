3e6c3f34c59fa5236416b11bc01009330aafc790
unknown <ahsanehsan00@gmail.com>
2025-01-28 17:11:10 +0500

    next

--- src/features/landing/home/statatics-section.tsx
+++ src/features/landing/home/statatics-section.tsx
@@ -3,26 +3,25 @@ import React from 'react';
 import CountUp from 'react-countup';
 
 const StataticsSection = () => {
-    return <>
+    return (
         <section className="statistics">
             <div className="container">
                 <div className="row">
                     <div className="col-lg-12">
-                        <ul className="states ps-0">
-                            <li><CountUp end={30} duration={3} className='bg-white'/>+ clients <br/> connected</li>
-                            <span></span>
-                            <li><CountUp end={90} duration={3} className='bg-white'/>+ developers <br/> hired</li>
-                            <span></span>
-                            <li><CountUp end={500} duration={3} className='bg-white'/>+ hours of <br/> recruitment saved</li>
-                            <span></span>
-                            <li className="d-flex align-items-center gap-4"> 100% current <br/> success rate <img
-                                src="/assets/img/graph.svg" alt="graph"/></li>
+                        <ul className="states ps-0 d-flex flex-column flex-md-row">
+                            <li className="mb-4 mb-md-0"><CountUp end={30} duration={3} className='bg-white' />+ clients <br /> connected</li>
+                            <span className="d-none d-md-block"></span>
+                            <li className="mb-4 mb-md-0"><CountUp end={90} duration={3} className='bg-white' />+ developers <br /> hired</li>
+                            <span className="d-none d-md-block"></span>
+                            <li className="mb-4 mb-md-0"><CountUp end={500} duration={3} className='bg-white' />+ hours of <br /> recruitment saved</li>
+                            <span className="d-none d-md-block"></span>
+                            <li className="d-flex align-items-center gap-4"> 100% current <br /> success rate <img src="/assets/img/graph.svg" alt="graph" /></li>
                         </ul>
                     </div>
                 </div>
             </div>
         </section>
-    </>
+    )
 }
 
-export default StataticsSection
+export default StataticsSection;
