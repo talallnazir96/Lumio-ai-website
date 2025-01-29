d203a154972c0945c7ed1b3a11594df49acc7e6d
Muhammad Kashan Haider <muhammadkashanhaider3241@gmail.com>
2025-01-29 15:59:52 +0500

    resolving deploy

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-    eslint: {
-        dirs: ['pages', 'utils'],
-    },
+  eslint: {
+    dirs: ['pages', 'utils'],
+  },
 };
 
 export default nextConfig;
